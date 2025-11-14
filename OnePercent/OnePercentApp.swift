511d6cc0ed53bcc273c7f4aa91eccf611f17a6e0
Lyon Li <lbl53@cornell.edu>
2025-11-13 16:58:07 -0800

    Initial Commit

---
+++ OnePercent/OnePercentApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnePercentApp.swift
+//  OnePercent
+//
+//  Created by Guest on 11/13/25.
+//
+
+import SwiftUI
+
+@main
+struct OnePercentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
